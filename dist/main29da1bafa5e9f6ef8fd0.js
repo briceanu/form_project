5622340df8eb37a4d20fd09cddad78c890b54e2d
Teodor <ursus_24@yahoo.com>
2022-10-30 15:51:10 +0200

    form and footer style add

--- dist/main29da1bafa5e9f6ef8fd0.js
+++ dist/main29da1bafa5e9f6ef8fd0.js
@@ -16,12 +16,20 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
 /* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/getUrl.js */ "./node_modules/css-loader/dist/runtime/getUrl.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__);
 // Imports
 
 
+
+var ___CSS_LOADER_URL_IMPORT_0___ = new URL(/* asset import */ __webpack_require__(/*! ./TradeWinds-Regular.ttf */ "./src/scss/TradeWinds-Regular.ttf"), __webpack_require__.b);
+var ___CSS_LOADER_URL_IMPORT_1___ = new URL(/* asset import */ __webpack_require__(/*! ../assets/mathematic.jpg */ "./src/assets/mathematic.jpg"), __webpack_require__.b);
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
+___CSS_LOADER_EXPORT___.push([module.id, "@import url(https://fonts.googleapis.com/css2?family=Abyssinica+SIL&family=Arvo:ital@1&family=Fascinate+Inline&family=Lemonada&family=Libre+Caslon+Display&family=Ms+Madi&display=swap);"]);
+var ___CSS_LOADER_URL_REPLACEMENT_0___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_0___);
+var ___CSS_LOADER_URL_REPLACEMENT_1___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_1___);
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "h1 {\n  color: rgba(194, 49, 49, 0.452);\n}", "",{"version":3,"sources":["webpack://./src/scss/style.scss","webpack://./src/scss/_variables.scss"],"names":[],"mappings":"AACA;EACE,+BCFU;ADEZ","sourcesContent":["@use \"_variables\" as *;\nh1 {\n  color: $light-red;\n}\n","$light-red: rgba(194, 49, 49, 0.452);\n"],"sourceRoot":""}]);
+___CSS_LOADER_EXPORT___.push([module.id, "@font-face {\n  font-family: \"TradeWinds-Regular\";\n  src: url(" + ___CSS_LOADER_URL_REPLACEMENT_0___ + ");\n}\n* {\n  margin: 0;\n  padding: 0;\n  box-sizing: border-box;\n}\n\nbody {\n  display: grid;\n  grid-template-columns: repeat(2, 40% 60%);\n  grid-template-rows: repeat(1, 100%);\n}\n\n.logo-img {\n  width: 150px;\n  height: 150px;\n}\n\n.logo-container {\n  background-image: url(" + ___CSS_LOADER_URL_REPLACEMENT_1___ + ");\n  background-position: center;\n  display: flex;\n  flex-direction: column;\n  padding-block-start: 26rem;\n}\n\n.logo {\n  background-color: rgba(22, 12, 32, 0.164);\n  display: flex;\n  justify-content: center;\n  align-items: center;\n}\n.logo h1 {\n  color: rgb(255, 255, 255);\n  font-family: \"TradeWinds-Regular\";\n  font-weight: 900;\n  font-size: 3rem;\n}\n\n.motivation {\n  background-color: rgba(225, 227, 230, 0.418);\n  box-shadow: 0 5px 5px rgba(4, 31, 95, 0.5);\n}\n\nh2 {\n  padding-block-start: 3rem;\n  width: max(60%, 200px);\n  margin-left: 40px;\n  font-family: \"TradeWinds-Regular\";\n  text-align: justify;\n  font-size: 1.6rem;\n}\n\n.second-h2 {\n  padding-block-end: 1rem;\n}\n\n.form-container h1,\n.form-container p {\n  margin-left: 40px;\n}\n.form-container p {\n  color: rgb(90, 91, 94);\n  font-weight: 800;\n  font-size: 1.2rem;\n}\n\nspan {\n  color: red;\n}\n\nform label {\n  width: 200px;\n  display: inline-block;\n  text-transform: uppercase;\n  font-size: 1rem;\n  font-weight: 700;\n}\nform input {\n  width: 220px;\n  height: 45px;\n  margin: 0.4rem 0;\n  border: none;\n  border-bottom: 2px solid rgb(184, 187, 189);\n  outline: none;\n  font-size: 1.3rem;\n  font-family: \"Abyssinica SIL\", serif;\n}\nform input::placeholder {\n  font-size: 1.2rem;\n  font-family: \"Libre Caslon Display\", serif;\n}\nform input:focus {\n  border-bottom: 4px solid rgb(103, 144, 170);\n}\n\nform[method=get] {\n  display: flex;\n  flex-direction: row;\n  margin: 20px 40px;\n}\n\nlabel {\n  padding: 1rem 0;\n}\n\n.left {\n  display: flex;\n  flex-direction: column;\n  flex-wrap: wrap;\n}\n\n.right {\n  display: flex;\n  flex-direction: column;\n  margin-left: 10rem;\n  flex-wrap: wrap;\n}\n\n.form-container {\n  box-shadow: 0 5px 5px rgba(4, 31, 95, 0.5);\n}\n.form-container h1 {\n  margin-block-start: 1rem;\n}\n\n.submit-section {\n  margin-left: 40px;\n}\n.submit-section a {\n  color: rgb(103, 144, 170);\n  display: inline-block;\n}\n.submit-section p {\n  font-size: 1.2rem;\n  padding-block-end: 0.4rem;\n}\n\nbutton {\n  padding: 0.5rem 1.4rem;\n  background-color: rgba(7, 7, 75, 0);\n  color: rgb(43, 41, 41);\n  border-radius: 10px;\n  border: 3px solid rgb(103, 144, 170);\n  font-size: 1.2rem;\n  margin-block-end: 0.4rem;\n  text-transform: uppercase;\n}\nbutton:hover {\n  cursor: pointer;\n  color: white;\n  background-color: rgb(103, 144, 170);\n}\n\ninput:focus::placeholder {\n  transform: translateY(-13px);\n  scale: 0.9;\n  transition: all 300ms linear;\n}\n\ninput[type=email]:invalid {\n  background-color: rgba(255, 0, 0, 0.329);\n}\n\ninput[type=email]:valid {\n  background-color: rgba(50, 205, 50, 0.466);\n}\n\nfooter {\n  background-color: rgba(22, 12, 32, 0.164);\n  display: flex;\n  flex-direction: row;\n  justify-content: center;\n  align-items: center;\n  flex-wrap: wrap;\n}\nfooter p {\n  font-size: 1.2rem;\n  padding: 0 0.1rem;\n}\n\n.gitImg {\n  width: 40px;\n  height: 40px;\n}", "",{"version":3,"sources":["webpack://./src/scss/_variables.scss","webpack://./src/scss/style.scss"],"names":[],"mappings":"AAKA;EACE,iCAAA;EACA,4CAAA;ACHF;AAHA;EACE,SAAA;EACA,UAAA;EACA,sBAAA;AAKF;;AAHA;EACE,aAAA;EACA,yCAAA;EACA,mCAAA;AAMF;;AAHA;EACE,YAAA;EACA,aAAA;AAMF;;AAJA;EACE,yDAAA;EACA,2BAAA;EACA,aAAA;EACA,sBAAA;EACA,0BAAA;AAOF;;AALA;EACE,yCAAA;EACA,aAAA;EACA,uBAAA;EACA,mBAAA;AAQF;AAPE;EACE,yBD5BQ;EC6BR,iCAAA;EACA,gBAAA;EACA,eAAA;AASJ;;AAJA;EACE,4CAAA;EACA,0CAAA;AAOF;;AALA;EACE,yBAAA;EACA,sBAAA;EACA,iBAAA;EACA,iCAAA;EACA,mBAAA;EACA,iBAAA;AAQF;;AANA;EACE,uBAAA;AASF;;AANE;;EAEE,iBAAA;AASJ;AANE;EACE,sBDzDS;EC0DT,gBAAA;EACA,iBAAA;AAQJ;;AAJA;EACE,UAAA;AAOF;;AAJE;EACE,YAAA;EACA,qBAAA;EACA,yBAAA;EACA,eAAA;EACA,gBAAA;AAOJ;AALE;EACE,YAAA;EACA,YAAA;EACA,gBAAA;EACA,YAAA;EACA,2CAAA;EACA,aAAA;EACA,iBAAA;EACA,oCDhFO;ACuFX;AALI;EACE,iBAAA;EACA,0CDrFO;AC4Fb;AAJE;EACE,2CAAA;AAMJ;;AAFA;EACE,aAAA;EACA,mBAAA;EACA,iBAAA;AAKF;;AAHA;EACE,eAAA;AAMF;;AAHA;EACE,aAAA;EACA,sBAAA;EACA,eAAA;AAMF;;AAJA;EACE,aAAA;EACA,sBAAA;EACA,kBAAA;EACA,eAAA;AAOF;;AALA;EAIE,0CAAA;AAKF;AARE;EACE,wBAAA;AAUJ;;AANA;EACE,iBAAA;AASF;AARE;EACE,yBAAA;EACA,qBAAA;AAUJ;AAPE;EACE,iBAAA;EACA,yBAAA;AASJ;;AANA;EACE,sBAAA;EACA,mCAAA;EACA,sBAAA;EACA,mBAAA;EACA,oCAAA;EACA,iBAAA;EACA,wBAAA;EACA,yBAAA;AASF;AARE;EACE,eAAA;EACA,YAAA;EACA,oCAAA;AAUJ;;AAPA;EACE,4BAAA;EACA,UAAA;EACA,4BAAA;AAUF;;AAPA;EACE,wCAAA;AAUF;;AARA;EACE,0CAAA;AAWF;;AATA;EACE,yCAAA;EACA,aAAA;EACA,mBAAA;EACA,uBAAA;EACA,mBAAA;EACA,eAAA;AAYF;AAVE;EACE,iBAAA;EACA,iBAAA;AAYJ;;AATA;EACE,WAAA;EACA,YAAA;AAYF","sourcesContent":["@import url(\"https://fonts.googleapis.com/css2?family=Abyssinica+SIL&family=Arvo:ital@1&family=Fascinate+Inline&family=Lemonada&family=Libre+Caslon+Display&family=Ms+Madi&display=swap\");\n$clr-light: rgb(255, 255, 255);\n$light-gray: rgb(90, 91, 94);\n$font-libre: \"Libre Caslon Display\", serif;\n$font-aby: \"Abyssinica SIL\", serif;\n@font-face {\n  font-family: \"TradeWinds-Regular\";\n  src: url(./TradeWinds-Regular.ttf);\n}\n","@use \"_variables\" as *;\n* {\n  margin: 0;\n  padding: 0;\n  box-sizing: border-box;\n}\nbody {\n  display: grid;\n  grid-template-columns: repeat(2, 40% 60%);\n  grid-template-rows: repeat(1, 100%);\n}\n//beginning of the logo side\n.logo-img {\n  width: 150px;\n  height: 150px;\n}\n.logo-container {\n  background-image: url(../assets/mathematic.jpg);\n  background-position: center;\n  display: flex;\n  flex-direction: column;\n  padding-block-start: 26rem;\n}\n.logo {\n  background-color: rgba(22, 12, 32, 0.164);\n  display: flex;\n  justify-content: center;\n  align-items: center;\n  h1 {\n    color: $clr-light;\n    font-family: \"TradeWinds-Regular\";\n    font-weight: 900;\n    font-size: 3rem;\n  }\n}\n//end of the logo style\n\n.motivation {\n  background-color: rgba(225, 227, 230, 0.418);\n  box-shadow: 0 5px 5px rgb(4, 31, 95, 0.5);\n}\nh2 {\n  padding-block-start: 3rem;\n  width: max(60%, 200px);\n  margin-left: 40px;\n  font-family: \"TradeWinds-Regular\";\n  text-align: justify;\n  font-size: 1.6rem;\n}\n.second-h2 {\n  padding-block-end: 1rem;\n}\n.form-container {\n  h1,\n  p {\n    margin-left: 40px;\n  }\n\n  p {\n    color: $light-gray;\n    font-weight: 800;\n    font-size: 1.2rem;\n  }\n}\n//styling form\nspan {\n  color: red;\n}\nform {\n  label {\n    width: 200px;\n    display: inline-block;\n    text-transform: uppercase;\n    font-size: 1rem;\n    font-weight: 700;\n  }\n  input {\n    width: 220px;\n    height: 45px;\n    margin: 0.4rem 0;\n    border: none;\n    border-bottom: 2px solid rgb(184, 187, 189);\n    outline: none;\n    font-size: 1.3rem;\n    font-family: $font-aby;\n\n    &::placeholder {\n      font-size: 1.2rem;\n      font-family: $font-libre;\n    }\n  }\n  input:focus {\n    border-bottom: 4px solid rgb(103, 144, 170);\n    // animation: move 200ms linear;\n  }\n}\nform[method=\"get\"] {\n  display: flex;\n  flex-direction: row;\n  margin: 20px 40px;\n}\nlabel {\n  padding: 1rem 0;\n}\n\n.left {\n  display: flex;\n  flex-direction: column;\n  flex-wrap: wrap;\n}\n.right {\n  display: flex;\n  flex-direction: column;\n  margin-left: 10rem;\n  flex-wrap: wrap;\n}\n.form-container {\n  h1 {\n    margin-block-start: 1rem;\n  }\n  box-shadow: 0 5px 5px rgb(4, 31, 95, 0.5);\n}\n.submit-section {\n  margin-left: 40px;\n  a {\n    color: rgb(103, 144, 170);\n    display: inline-block;\n  }\n\n  p {\n    font-size: 1.2rem;\n    padding-block-end: 0.4rem;\n  }\n}\nbutton {\n  padding: 0.5rem 1.4rem;\n  background-color: rgba(7, 7, 75, 0);\n  color: rgb(43, 41, 41);\n  border-radius: 10px;\n  border: 3px solid rgb(103, 144, 170);\n  font-size: 1.2rem;\n  margin-block-end: 0.4rem;\n  text-transform: uppercase;\n  &:hover {\n    cursor: pointer;\n    color: white;\n    background-color: rgb(103, 144, 170);\n  }\n}\ninput:focus::placeholder {\n  transform: translateY(-13px);\n  scale: 0.9;\n  transition: all 300ms linear;\n}\n\ninput[type=\"email\"]:invalid {\n  background-color: rgba(255, 0, 0, 0.329);\n}\ninput[type=\"email\"]:valid {\n  background-color: rgba(50, 205, 50, 0.466);\n}\nfooter {\n  background-color: rgba(22, 12, 32, 0.164);\n  display: flex;\n  flex-direction: row;\n  justify-content: center;\n  align-items: center;\n  flex-wrap: wrap;\n\n  p {\n    font-size: 1.2rem;\n    padding: 0 0.1rem;\n  }\n}\n.gitImg {\n  width: 40px;\n  height: 40px;\n}\n"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -135,6 +143,44 @@ module.exports = function (cssWithMappingToString) {
   };
 
   return list;
+};
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/dist/runtime/getUrl.js":
+/*!********************************************************!*\
+  !*** ./node_modules/css-loader/dist/runtime/getUrl.js ***!
+  \********************************************************/
+/***/ ((module) => {
+
+
+
+module.exports = function (url, options) {
+  if (!options) {
+    options = {};
+  }
+
+  if (!url) {
+    return url;
+  }
+
+  url = String(url.__esModule ? url.default : url); // If url is already wrapped in quotes, remove them
+
+  if (/^['"].*['"]$/.test(url)) {
+    url = url.slice(1, -1);
+  }
+
+  if (options.hash) {
+    url += options.hash;
+  } // Should url be wrapped?
+  // See https://drafts.csswg.org/css-values-3/#urls
+
+
+  if (/["'() \t\n]|(%20)/.test(url) || options.needQuotes) {
+    return "\"".concat(url.replace(/"/g, '\\"').replace(/\n/g, "\\n"), "\"");
+  }
+
+  return url;
 };
 
 /***/ }),
@@ -538,6 +584,36 @@ module.exports = styleTagTransform;
 
 module.exports = __webpack_require__.p + "gitIcon.svg";
 
+/***/ }),
+
+/***/ "./src/assets/mathematic.jpg":
+/*!***********************************!*\
+  !*** ./src/assets/mathematic.jpg ***!
+  \***********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "mathematic.jpg";
+
+/***/ }),
+
+/***/ "./src/assets/odin-lined.png":
+/*!***********************************!*\
+  !*** ./src/assets/odin-lined.png ***!
+  \***********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "odin-lined.png";
+
+/***/ }),
+
+/***/ "./src/scss/TradeWinds-Regular.ttf":
+/*!*****************************************!*\
+  !*** ./src/scss/TradeWinds-Regular.ttf ***!
+  \*****************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "TradeWinds-Regular.ttf";
+
 /***/ })
 
 /******/ 	});
@@ -565,6 +641,9 @@ module.exports = __webpack_require__.p + "gitIcon.svg";
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/compat get default export */
@@ -639,6 +718,32 @@ module.exports = __webpack_require__.p + "gitIcon.svg";
 /******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		__webpack_require__.b = document.baseURI || self.location.href;
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"main": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		// no on chunks loaded
+/******/ 		
+/******/ 		// no jsonp function
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/nonce */
 /******/ 	(() => {
 /******/ 		__webpack_require__.nc = undefined;
@@ -653,14 +758,18 @@ var __webpack_exports__ = {};
   \*************************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _scss_style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/style.scss */ "./src/scss/style.scss");
-/* harmony import */ var _assets_gitIcon_svg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../assets/gitIcon.svg */ "./src/assets/gitIcon.svg");
+/* harmony import */ var _assets_odin_lined_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../assets/odin-lined.png */ "./src/assets/odin-lined.png");
+/* harmony import */ var _assets_gitIcon_svg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../assets/gitIcon.svg */ "./src/assets/gitIcon.svg");
 
 
-const img = document.getElementById("picture");
-img.src = _assets_gitIcon_svg__WEBPACK_IMPORTED_MODULE_1__;
+
+const img = document.querySelector(".logo-img");
+img.src = _assets_odin_lined_png__WEBPACK_IMPORTED_MODULE_1__;
+const git = document.querySelector(".gitImg");
+git.src = _assets_gitIcon_svg__WEBPACK_IMPORTED_MODULE_2__;
 
 })();
 
 /******/ })()
 ;
-//# sourceMappingURL=main4598c67c51fd4356cef3.js.map
+//# sourceMappingURL=main29da1bafa5e9f6ef8fd0.js.map
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
 import style from "../scss/style.scss";
 import odinImg from "../assets/odin-lined.png";
+import gitIcon from "../assets/gitIcon.svg";
 const img = document.querySelector(".logo-img");
 img.src = odinImg;
+const git = document.querySelector(".gitImg");
+git.src = gitIcon;
